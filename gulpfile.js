949e8063cb55a5b3b4d65bad419a4bee0a3080a2
Yoshiya Hinosawa <stibium121@gmail.com>
2014-05-29 15:16:45 +0900

    Add mocha task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,27 @@
 
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var exec = require('child_process').exec;
+
 
 gulp.task('jshint', function () {
+
     return gulp.src('{./,src/,bin/,test/}*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
+
 });
 
-gulp.task('default', ['jshint']);
+gulp.task('mocha', ['jshint'], function (cb) {
+
+    exec('mocha -R spec test/', function (err, stdout, stderr) {
+
+        if (stdout) console.log(stdout);
+        if (stderr) console.log(stderr);
+
+        cb(err);
+    });
+
+});
+
+gulp.task('default', ['mocha']);
